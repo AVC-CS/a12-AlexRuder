dd411e6140172ec3c63d7ae4b91e5319df4913b3
AlexRuder <aruder@students.avc.edu>
2026-02-25 11:14:34 -0800

    Update t.cpp

--- t.cpp
+++ t.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
+int globlInt = 100;
 
+int main (){
+  int mun = 10;
+  cout << "address of num" << &num << endl;
+  cout << "address of globlInt" << &globlInt << endl;
+  cout << "address of staticInt" << &staticInt << endl;
+}
